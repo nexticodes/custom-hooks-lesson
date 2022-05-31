9f22be3e2f5c653d58bbf86645af12e087422300
Ernest Bruno <erndobruno@gmail.com>
2022-05-31 11:25:49 -0700

    Add axios, finish Random component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
+import Random from './components/Random_V1';
+import Tag from './components/Tag_V1';
 function App() {
-  return (
-    <div className="App">
-      <h1>Random Giphy Website</h1>
-    </div>
-  );
+
+	return (
+		<div>
+			<h1>Random Giphy Website</h1>
+			<div className="main-container">
+				<Random />
+				<Tag />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Tag_V1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tag_V1 = () => {
+  
+	return (
+		<>
+			<h1>Tag</h1>
+		</>
+	);
+};
+
+export default Tag_V1;
